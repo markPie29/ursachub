9be833b4898c5353fb044477bd45e9392275e67e
Tetetsz <althealizettepalustre@gmail.com>
2024-11-10 17:57:08 +0800

    homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,11 +2,24 @@
 
 @section('content')
 
-    <body>    
-        <img class="hero-bg" src="URSAC.png" alt="">
-        <div class="hero-text">
-            <h1>URSAC Hub</h1>
-        </div>
-    </body>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>URSAC HUB</title>
+</head>
+<body>
+
+<div class="background">
+    <div class="content">
+        <div class="title">URSAC HUB</div>
+        <div class="subtitle">Your One-Stop Shop for Campus Merchandise and Updates</div>
+    </div>
+</div>
+
+</body>
+</html>
+
 
 @endsection
