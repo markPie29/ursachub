6643e02c05bfe1d3bd7ef9ed4875c9da31f87f1a
EngrPrenz <153071661+EngrPrenz@users.noreply.github.com>
2024-11-11 18:04:35 +0800

    Revert "bago to guys frontend"

--- resources/views/show_eachnews.blade.php
+++ resources/views/show_eachnews.blade.php
@@ -5,29 +5,24 @@
 
 </section>
 
-<div class="container">
-    <div class="row">
-        <div class="col-md-6">
-            <h2>{{ $news->name }}</h2>
-            <p>{{ $news->org }}</p>
-            
-            <!-- Render HTML content safely -->
-            <div>
-                {!! $news->content !!}
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <h2>{{ $news->name }}</h2>
+                <p>{{ $news->org }}</p>
+                <p>{{ $news->content }}</p>
+                @php
+                    $photos = json_decode($news->photos, true); // Decode the JSON column to an array
+                @endphp
+
+                @if(is_array($photos) && count($photos) > 0)
+                    @foreach($photos as $photo)
+                        <img src="{{ asset('storage/' . $photo) }}" alt="Product Photo" style="max-width: 100%; height: auto;">
+                    @endforeach
+                @else
+                    <p>No images available</p>
+                @endif
             </div>
-
-            @php
-                $photos = json_decode($news->photos, true); // Decode the JSON column to an array
-            @endphp
-
-            @if(is_array($photos) && count($photos) > 0)
-                @foreach($photos as $photo)
-                    <img src="{{ asset('storage/' . $photo) }}" alt="Product Photo" style="max-width: 100%; height: auto;">
-                @endforeach
-            @else
-                <p>No images available</p>
-            @endif
         </div>
     </div>
-</div>
 @endsection
