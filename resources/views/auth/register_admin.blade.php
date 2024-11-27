040b0d700dd541a90ad0b70b8368bdc051dbcf71
EngrPrenz <153071661+EngrPrenz@users.noreply.github.com>
2024-11-27 22:09:59 +0800

    grey text sa registrations

--- resources/views/auth/register_admin.blade.php
+++ resources/views/auth/register_admin.blade.php
@@ -7,11 +7,11 @@
 
         <h2>Admin Registration</h2 class="text-1-admin">
         <div class="form-group">
-            <label for="name">Name</label>
+            <label for="name" class="label-registration">Name</label>
             <input type="text" class="form-control" id="name" name="name" style="text-transform:uppercase" required>
         </div>
         <div class="form-group">
-            <label for="org">Organization</label>
+            <label for="org" class="label-registration">Organization</label>
             <select class="form-control" id="org" name="org" required>
                 <option value="" disabled selected>Select an organization</option>
                 @foreach ($organizations as $organization)
@@ -20,11 +20,11 @@
             </select>
         </div>
         <div class="form-group">
-            <label for="password">Password</label>
+            <label for="password" class="label-registration">Password</label>
             <input type="password" class="form-control" id="password" name="password" required>
         </div>
         <div class="form-group">
-            <label for="password_confirmation">Confirm Password</label>
+            <label for="password_confirmation" class="label-registration">Confirm Password</label>
             <input type="password" class="form-control" id="password_confirmation" name="password_confirmation" required>
         </div>
         <a href="#" onclick="showPasswordwC()" class="show-password">Show Password</a>
--- resources/views/auth/register_student.blade.php
+++ resources/views/auth/register_student.blade.php
@@ -7,23 +7,23 @@
         @csrf
         <div class="form-group">
             <h2>Student Registration</h2>
-            <label for="first_name">First Name</label>
+            <label for="first_name" class="label-registration">First Name</label>
             <input type="text" class="form-control" id="first_name" name="first_name" style="text-transform:uppercase" required>
         </div>
         <div class="form-group">
-            <label for="last_name">Last Name</label>
+            <label for="last_name" class="label-registration" >Last Name</label>
             <input type="text" class="form-control" id="last_name" name="last_name" style="text-transform:uppercase" required>
         </div>
         <div class="form-group">
-            <label for="middle_name">Middle Name</label>
+            <label for="middle_name" class="label-registration">Middle Name</label>
             <input type="text" class="form-control" id="middle_name" name="middle_name" style="text-transform:uppercase">
         </div>
         <div class="form-group">
-            <label for="student_id">Student ID</label>
+            <label for="student_id" class="label-registration">Student ID</label>
             <input type="text" class="form-control" id="student_id" name="student_id" required>
         </div>
         <div class="form-group">
-            <label for="course">Program</label>
+            <label for="course" class="label-registration">Program</label>
 
             <select class="form-control" id="course" name="course_id" required>
                 <option value="" disabled selected>Select your program</option>
@@ -35,11 +35,11 @@
 
         </div>
         <div class="form-group">
-            <label for="password">Password</label>
+            <label for="password" class="label-registration">Password</label>
             <input type="password" class="form-control" id="password" name="password" required>
         </div>
         <div class="form-group">
-            <label for="password_confirmation">Confirm Password</label>
+            <label for="password_confirmation" class="label-registration">Confirm Password</label>
             <input type="password" class="form-control" id="password_confirmation" name="password_confirmation" required>
         </div>
 
